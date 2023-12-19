75876184c255f572458dee4c54614d1b3bb389a4
Brandon <brandon@tendency.me>
2023-12-19 02:07:51 -0600

    ✨ Create a generic Home component

---
+++ app/Livewire/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Home extends Component
+{
+    /**
+     * Render the component.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function render()
+    {
+        return view('livewire.home');
+    }
+}
