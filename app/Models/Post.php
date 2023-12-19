0d9b0c17eaebfab2b762ef4949f5f83c67ba46e3
Brandon <brandon@tendency.me>
2023-12-19 03:42:36 -0600

    🗃️ Create a generic Post model and migration

---
+++ app/Models/Post.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'user_id',
+        'is_published',
+        'published_at',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'content' => 'array',
+        'is_published' => 'boolean',
+        'published_at' => 'datetime',
+    ];
+
+    /**
+     * Get the user that owns the post.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Retrieve the post URL.
+     *
+     * @return string
+     */
+    public function getUrlAttribute()
+    {
+        return route('post.show', $this);
+    }
+
+    /**
+     * Retrieve the post content as an object.
+     *
+     * @return object
+     */
+    public function getBlocksAttribute()
+    {
+        return json_decode(
+            collect($this->content ?? [])->toJson()
+        );
+    }
+
+    /**
+     * Retrieve the published posts.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopePublished($query)
+    {
+        return $query->where('is_published', true);
+    }
+}
---
+++ database/migrations/2023_12_19_082215_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique()->index();
+            $table->json('content')->nullable();
+            $table->foreignId('user_id')->constrained('users');
+            $table->boolean('is_published')->default(false);
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
