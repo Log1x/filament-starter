24bd7da8d4b7f4a0c32da5cd3982aed30b893aa8
Brandon <brandon@tendency.me>
2023-12-19 02:08:54 -0600

    🔧 Use the new Home component for the `/` route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class)->name('home');
