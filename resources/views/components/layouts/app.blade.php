c08bacf0072413cdd276ec24b3527606bdf1651a
Brandon <brandon@tendency.me>
2023-12-19 03:49:13 -0600

    💄 Add default container styling to the app layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,7 +12,7 @@
   </head>
 
   <body class="text-base leading-normal tracking-normal text-gray-800 transition-colors">
-    <div>
+    <div class="max-w-lg mx-auto py-8 px-4">
       {{ $slot }}
     </div>
 
