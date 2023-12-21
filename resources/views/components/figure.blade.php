b309775485c72c4f58e858bbf84e3e4b0cebbddc
Brandon <brandon@tendency.me>
2023-12-21 06:16:04 -0600

    🎨 Add a conditional before querying the media library in the figure component

--- resources/views/components/figure.blade.php
+++ resources/views/components/figure.blade.php
@@ -3,7 +3,7 @@
 <figure>
   <img
     class="object-cover w-full border rounded-lg shadow"
-    src="{{ Awcodes\Curator\Models\Media::find($image)?->url ?? $image }}"
+    src="{{ is_numeric($image) ? Awcodes\Curator\Models\Media::find($image)?->url : $image }}"
     alt="{{ $alt }}"
   >
 
