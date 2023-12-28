3019dbff20530a4268d456b7a1625864e28246d5
Brandon <brandon@tendency.me>
2023-12-28 02:39:25 -0600

    💄 Restyle the post list into cards utilizing the post image

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -2,29 +2,43 @@
   <x-hero title="Home" />
 
   <x-container>
-    <h2 class="mb-6 text-4xl">
+    <h2 class="mb-8 text-4xl">
       Latest Posts
     </h2>
 
-    <ul class="ml-4 space-y-1 list-disc">
+    <div class="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
       @foreach ($posts as $post)
-        <li>
+        <div>
           <a
-            class="text-primary-500 transition-colors hover:text-primary-600"
+            class="*:transition group"
             href="{{ $post->url }}"
             wire:navigate
           >
-            {{ $post->title }}
+            <div class="w-full h-48 mb-2 overflow-hidden border rounded-lg group-hover:brightness-90">
+              @if ($post->image)
+                <img
+                  class="object-cover w-full h-full"
+                  src="{{ $post->image->url }}"
+                  alt="{{ $post->image->alt ?? $post->title }}"
+                />
+              @else
+                <div class="flex items-center justify-center w-full h-full text-gray-400 bg-gray-200">
+                  Article
+                </div>
+              @endif
+            </div>
+
+            <h3 class="text-lg text-gray-700 group-hover:text-primary-500">
+              {{ $post->title }}
+            </h3>
           </a>
-        </li>
+        </div>
       @endforeach
 
       @empty($posts)
-        <li class="text-gray-500">
-          No posts yet.
-        </li>
+        <div>No posts yet.</div>
       @endempty
-    </ul>
+    </div>
 
     @if ($posts->hasPages())
       <div class="pt-6 mt-6 border-t">
