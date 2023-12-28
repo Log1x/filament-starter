03571ad82189eb7d549fc7e8ff218a10563ec92a
Brandon <brandon@tendency.me>
2023-12-28 02:34:34 -0600

    🗃️ Add `image_id` to the posts table

---
+++ database/migrations/2023_12_28_074322_add_image_to_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table
+                ->foreignId('image_id')
+                ->after('content')
+                ->nullable()
+                ->constrained('media')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('image_id');
+        });
+    }
+};
